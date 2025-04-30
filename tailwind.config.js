d9e10ee06e90a801687dd4f2ada197a05444a5e3
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-30 14:47:34 +0200

    removed body styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,43 +10,7 @@ export default {
       },
     },
   },
-  plugins: [
-    plugin(function ({ addBase }) {
-      addBase({
-        body: {
-          "@apply mx-auto w-full max-w-screen-2xl bg-neutral-900 font-body font-normal text-white":
-            {},
-        },
-        "h1, .h1, h2, .h2, h3, .h3, h4, .h4, h5, .h5, h6, .h6": {
-          "@apply mb-4 mt-0 font-body font-heading leading-none": {},
-        },
-
-        "h1, .h1": {
-          "@apply text-4xl md:text-5xl leading-10": {},
-        },
-
-        "h2, .h2": {
-          "@apply text-3xl md:text-4xl leading-9": {},
-        },
-
-        "h3, .h3": {
-          "@apply text-2xl md:text-3xl leading-8": {},
-        },
-
-        "h4, .h4": {
-          "@apply text-xl md:text-2xl leading-7": {},
-        },
-
-        "h5, .h5": {
-          "@apply text-lg md:text-xl leading-6": {},
-        },
-
-        "h6, .h6": {
-          "@apply text-base md:text-lg leading-5": {},
-        },
-      });
-    }),
-  ],
+  plugins: [],
   corePlugins: {
     preflight: true,
   },
