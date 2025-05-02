47ad797bf92b0142193f7fccf82bd0a43a35ad70
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-02 16:57:28 +0200

    trying the loader.js approach instead of embed

---
+++ loader.js
@@ -0,0 +1,39 @@
+(function () {
+  const appContainer = document.createElement("div");
+  appContainer.id = "app";
+  document.body.appendChild(appContainer);
+
+  const appUrl =
+    "https://cdn.jsdelivr.net/gh/ArtMooney/finanslaget@v0.0.1/dist";
+  const apiBase = "https://finanslaget.pages.dev";
+  window.apiBase = apiBase;
+
+  fetch(appUrl + "/index.html")
+    .then((response) => response.text())
+    .then((html) => {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(html, "text/html");
+
+      const scriptSrc = htmlDoc
+        .querySelector('script[src*=".js"]')
+        ?.getAttribute("src");
+      const cssSrc = htmlDoc
+        .querySelector('link[rel="stylesheet"]')
+        ?.getAttribute("href");
+
+      if (scriptSrc) {
+        const scriptElement = document.createElement("script");
+        scriptElement.type = "module";
+        scriptElement.src = appUrl + scriptSrc;
+        document.head.appendChild(scriptElement);
+      }
+
+      if (cssSrc) {
+        const linkElement = document.createElement("link");
+        linkElement.rel = "stylesheet";
+        linkElement.href = appUrl + cssSrc;
+        document.head.appendChild(linkElement);
+      }
+    })
+    .catch((error) => console.error("Error loading app:", error));
+})();
