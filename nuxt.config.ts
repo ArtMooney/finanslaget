be76bd8efb9dbe5bb79ffb9277333a775725255b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-04 00:15:23 +0200

    restored some of the standard nuxt settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,37 +5,17 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-04-28",
   devtools: { enabled: true },
   css: ["/assets/css/main.css"],
-
-  ssr: false,
-  app: {
-    baseURL: "/",
-    buildAssetsDir: "/_nuxt/",
-  },
-
   vite: {
     plugins: [tailwindcss()],
-    build: {
-      assetsInlineLimit: 51200,
-      rollupOptions: {
-        output: {
-          manualChunks: () => "app.js",
-        },
-      },
-    },
   },
-
   nitro: {
     preset: "cloudflare-pages",
-    output: {
-      singleChunk: true,
-    },
     prerender: {
       crawlLinks: true,
       routes: ["/"],
       ignore: [],
     },
   },
-
   runtimeConfig: {
     mailgunApiKey: process.env.NUXT_MAILGUN_API_KEY,
     emailFrom: process.env.NUXT_EMAIL_FROM,
@@ -46,13 +26,9 @@ export default defineNuxtConfig({
     public: {
       userName: process.env.NUXT_PUBLIC_USERNAME,
       userPass: process.env.NUXT_PUBLIC_USERPASS,
-      baseUrl: "/",
-      apiBase: "https://finanslaget.pages.dev",
     },
   },
-
-  modules: ["@nuxt/image", "@nuxt/icon"],
-
+  modules: ["@nuxtjs/robots", "@nuxtjs/sitemap", "@nuxt/image", "@nuxt/icon"],
   image: {
     dir: "assets/images",
     format: ["webp", "jpg", "png"],
@@ -74,5 +50,20 @@ export default defineNuxtConfig({
     },
     staticFilename: "[name]-[width]-[height]-[format].[ext]",
     provider: "ipxStatic",
+  },
+  robots: {
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/",
+        comment: "Disallow all robots on .pages.dev and its subdomains",
+      },
+    ],
+    disallowNonStandardSchemes: true,
+    sitemap: "",
+  },
+  sitemap: {
+    hostname: "",
+    gzip: true,
   },
 });
