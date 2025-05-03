6375d7fc1beccdaddefd3384164ad2d11c31b5d9
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-04 00:49:03 +0200

    more work on iframe handling plugin

--- plugins/embed.js
+++ plugins/embed.js
@@ -1,42 +1,42 @@
 export default defineNuxtPlugin(() => {
   if (process.client && window.parent !== window) {
-    const debounce = (func, delay) => {
-      let timer;
-      return function (...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => func.apply(this, args), delay);
-      };
-    };
-
     const updateHeight = () => {
-      document.body.offsetHeight;
+      setTimeout(() => {
+        const height = Math.max(
+          document.body.scrollHeight,
+          document.documentElement.scrollHeight,
+          document.body.offsetHeight,
+          document.documentElement.offsetHeight,
+        );
 
-      const height = Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-      );
-
-      window.parent.postMessage(
-        {
-          type: "resize",
-          height: height,
-          source: "finanslaget-app",
-        },
-        "*",
-      );
+        window.parent.postMessage(
+          {
+            type: "resize",
+            height: height,
+            source: "finanslaget-app",
+          },
+          "*",
+        );
+      }, 50);
     };
-
-    const debouncedUpdateHeight = debounce(updateHeight, 100);
 
     setTimeout(updateHeight, 100);
     setTimeout(updateHeight, 500);
     setTimeout(updateHeight, 1000);
 
-    window.addEventListener("resize", debouncedUpdateHeight);
+    window.addEventListener("resize", () => {
+      window.parent.postMessage(
+        {
+          type: "reset-height",
+          source: "finanslaget-app",
+        },
+        "*",
+      );
+
+      updateHeight();
+    });
+
     window.addEventListener("load", updateHeight);
-    window.addEventListener("transitionend", updateHeight);
 
     const observer = new MutationObserver(updateHeight);
     observer.observe(document.body, {
