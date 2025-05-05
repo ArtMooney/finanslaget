c0c0f1172d5800fcf450770d4a4b2362e140e7a0
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-05 16:42:00 +0200

    updated the snabbkalkyl import script

--- public/finanslaget-snabbkalkyl-form.js
+++ public/finanslaget-snabbkalkyl-form.js
@@ -1,11 +1,22 @@
-// <script src="https://finanslaget.pages.dev/finanslaget-snabbkalkyl-form.js"></script>
+// <script src="https://finanslaget.pages.dev/finanslaget-snabbkalkyl-form.js?button_url=https%3A%2F%2Fbrave.se"></script>
 
 (function () {
   const currentScript = document.currentScript;
+  const scriptUrl = new URL(currentScript.src);
+  const buttonUrl = scriptUrl.searchParams.get("button_url");
+  const params = new URLSearchParams();
+
+  let iframeUrl = "https://finanslaget.pages.dev/snabbkalkyl";
+
+  if (buttonUrl) params.append("button_url", buttonUrl);
+
+  if (params.toString()) {
+    iframeUrl += `?${params.toString()}`;
+  }
 
   const iframe = document.createElement("iframe");
   iframe.id = "finanslaget-snabbkalkyl";
-  iframe.src = "https://finanslaget.pages.dev/snabbkalkyl";
+  iframe.src = iframeUrl;
   iframe.title = "Small form app to get price suggestions from Finanslaget";
   iframe.style.width = "100%";
   iframe.style.height = "0";
