1db313ffada3b71979c3ee07cd5c3bc5c5227419
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-05 11:35:58 +0200

    updated with hubspot form submisstion request

--- server/api/calculate.js
+++ server/api/calculate.js
@@ -1,6 +1,6 @@
 import { checkLogin } from "../utils/check-login.js";
-import { calculateMonthlyPayment } from "../utils/calculate-monthly-payment.js";
 import { calculateMultiplePeriods } from "../utils/calculate-multiple-periods.js";
+import { submitFormToHubspot } from "../utils/submit-form-to-hubspot.js";
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
@@ -16,9 +16,6 @@ export default defineEventHandler(async (event) => {
 
   const totalAmount = parseInt(body.amount);
 
-  // const noOfMonths = 60;
-  // const result = await calculateMonthlyPayment(totalAmount, noOfMonths, totalAmount * 0.1, totalAmount * 0.1);
-
   const result = await calculateMultiplePeriods(
     totalAmount,
     totalAmount * 0.1,
@@ -30,6 +27,23 @@ export default defineEventHandler(async (event) => {
       res.monthlyPayment = Math.round(res.monthlyPayment);
     }
   }
+
+  try {
+    const sendForm = await submitFormToHubspot(
+      config.portalId,
+      config.formId,
+      {
+        email: body.email,
+        mobilephone: body.phone,
+        organisationsnr: body.companyRegistrationNumber,
+        belopp: body.amount,
+        leasing_36_manader: result[0]?.monthlyPayment,
+        leasing_48_manader: result[1]?.monthlyPayment,
+        leasing_60_manader: result[2]?.monthlyPayment,
+      },
+      { pageUri: body.pageUri, pageName: body.pageName },
+    );
+  } catch (error) {}
 
   return result;
 });
