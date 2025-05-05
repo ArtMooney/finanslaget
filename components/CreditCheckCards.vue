bbf2633ffd400eab6e45a8324e819b8821020d4f
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-05-05 17:00:52 +0200

    added button to jump to link in query to results

--- components/CreditCheckCards.vue
+++ components/CreditCheckCards.vue
@@ -19,11 +19,24 @@
     I förslagen har vi räknat med 10% i en första förhöjd avgift samt 10% i
     slutvärde.
   </div>
+
+  <NuxtLink :to="$route.query.button_url || '#'" external>
+    <Button
+      text="Boka ett möte med rådgivare"
+      link=""
+      hash=""
+      type="button"
+      class="mt-6"
+    />
+  </NuxtLink>
 </template>
 
 <script>
+import Button from "~/components/elements/Button.vue";
+
 export default {
   name: "CreditCheckCards",
+  components: { Button },
 
   props: {
     apiData: {
@@ -35,7 +48,11 @@ export default {
 
   computed: {
     sortedApiData() {
-      return [...this.apiData].sort((a, b) => a.noOfMonths - b.noOfMonths);
+      if (Object.keys(this.apiData).length > 0) {
+        return [...this.apiData].sort((a, b) => a.noOfMonths - b.noOfMonths);
+      }
+
+      return [];
     },
   },
 };
